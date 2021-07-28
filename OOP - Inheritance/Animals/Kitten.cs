031264ba95421652f9a2c145a9b798f8f370363d
YaniStaykov <gp9060@gmail.com>
2021-07-28 21:16:33 +0300

    Add "Animals" task.

---
+++ OOP - Inheritance/Animals/Kitten.cs
@@ -0,0 +1,20 @@
+﻿namespace Animals
+{
+    public class Kitten : Animal, IProduceSound
+    {
+        public Kitten(string name, int age) 
+            : base(name, age, "Female")
+        {
+        }
+
+        public string ProduceSound()
+        {
+            return "Meow";
+        }
+
+        public override string GetResult()
+        {
+            return base.GetResult() + ProduceSound();
+        }
+    }
+}
