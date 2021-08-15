96a905d3e1e68a845f0973462fa05397b13057ca
YaniStaykov <gp9060@gmail.com>
2021-08-15 19:20:57 +0300

    Add MilitaryElite

---
+++ OOP - Interfaces and Abstraction/07.MilitaryElite/Interfaces/ISpecialisedSoldier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.MilitaryElite.Interfaces
+{
+    public interface ISpecialisedSoldier
+    {
+        public enum Corps
+        {
+            Airforces,
+            Marines
+        }
+        public interface ISpecialisedSoldier
+        {
+            Corps Corps { get; }
+        }
+    }
+}
