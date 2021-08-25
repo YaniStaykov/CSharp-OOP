6bd3450e94cf315aaac9590392d291fdfc443fc0
YaniStaykov <gp9060@gmail.com>
2021-08-25 23:42:44 +0300

    Add KnightsOfHonor

---
+++ Functional Programming/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _02.KnightsOfHonor
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                Action<string> appendSir = x => Console.WriteLine($"Sir {x}");
+
+                Console.ReadLine()
+                    .Split()
+                    .ToList()
+                    .ForEach(appendSir);
+            }
+    }
+}
---
+++ Functional Programming/02.KnightsOfHonor/README.md
@@ -0,0 +1,9 @@
+﻿# Knights Of Honor
+
+* This is a console application that reads a collection of names as strings from the console, appends "Sir" in front of every name and prints it back on the console.
+
+## Example 
+
+* Input:
+
+* Output:
