6a578e32fed2e468db6452bf39f07b3391f1e24e
YaniStaykov <gp9060@gmail.com>
2021-09-01 21:48:05 +0300

    Add AppliedArithmetics

---
+++ Functional Programming/05.AppliedArithmetics/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _05.AppliedArithmetics
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listOfNumbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (command == "end")
+                {
+                    break;
+                }
+
+                Func<int, int> manipulateList;
+
+                switch (command)
+                {
+                    case "add":
+                        manipulateList = x => x + 1;
+                        break;
+                    case "subtract":
+                        manipulateList = x => x - 1;
+                        break;
+                    case "multiply":
+                        manipulateList = x => x * 2;
+                        break;
+                    default:
+                        manipulateList = x => x;
+                        break;
+                }
+
+                listOfNumbers = listOfNumbers.Select(manipulateList).ToList();
+
+                if (command == "print")
+                {
+                    Console.WriteLine(string.Join(" ", listOfNumbers));
+                }
+            }
+        }
+    }
+}
