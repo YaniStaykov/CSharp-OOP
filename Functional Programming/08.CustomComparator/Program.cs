b26a586a1d5208ae2f979b8c8779e97d9e0b34bb
YaniStaykov <gp9060@gmail.com>
2021-09-06 21:25:59 +0300

    Add CustomComparator

---
+++ Functional Programming/08.CustomComparator/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _08.CustomComparator
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int> isEven = n => n % 2 == 0;
+
+            var numbers = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .GroupBy(n => isEven(n))
+                .OrderByDescending(g => g.Key)
+                .ToDictionary(g => g.Key, g => g.OrderBy(n => n).ToList());
+
+            foreach (var groupNumbers in numbers)
+            {
+                Console.Write(string.Join(" ", groupNumbers.Value) + " ");
+            }
+        }
+    }
+}
