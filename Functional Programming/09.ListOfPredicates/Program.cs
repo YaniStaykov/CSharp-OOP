671e2315e461633ec806afcfbf6a35e1c60751c3
YaniStaykov <gp9060@gmail.com>
2021-09-06 22:05:26 +0300

    Add ListOfPredicates

---
+++ Functional Programming/09.ListOfPredicates/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _09.ListOfPredicates
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var maxNumber = int.Parse(Console.ReadLine());
+            var dividers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            var listOfNumbers = new List<int>();
+            bool Filter(int x, int y) => x % y != 0;
+
+            for (int i = 1; i <= maxNumber; i++)
+            {
+                var isSearchNumber = true;
+
+                foreach (var divider in dividers)
+                {
+                    if (Filter(i, divider))
+                    {
+                        isSearchNumber = false;
+                        break;
+                    }
+                }
+
+                if (isSearchNumber)
+                {
+                    listOfNumbers.Add(i);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", listOfNumbers));
+        }
+    }
+}
---
+++ Functional Programming/09.ListOfPredicates/README.md
@@ -0,0 +1,10 @@
+﻿# ListOfPredicates
+
+* This is a console application that finds all numbers in the range 1...N that are divisible by the numbers of a given sequence.
+
+## Example 
+
+* Input: 10
+1 1 1 2
+
+* Output: 2 4 6 8 10
