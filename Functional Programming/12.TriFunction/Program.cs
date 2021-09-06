cb2ba63426bb8ff798ced1bd488d2f49f260c4ec
YaniStaykov <gp9060@gmail.com>
2021-09-06 23:35:43 +0300

    Add TriFunction

---
+++ Functional Programming/12.TriFunction/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _12.TriFunction
+{
+    using System;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Console.ReadLine()
+                .Split()
+                .FirstOrDefault(x => x.ToCharArray().Select(y => (int)y).Sum() >= n));
+        }
+    }
+}
