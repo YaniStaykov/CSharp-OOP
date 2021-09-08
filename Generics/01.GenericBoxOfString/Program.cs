836266e0a151d6a1bd5aad027c95016b0bb23cf7
YaniStaykov <gp9060@gmail.com>
2021-09-08 21:46:28 +0300

    Add GenericBoxOfString

---
+++ Generics/01.GenericBoxOfString/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _01.GenericBoxOfString
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string value = Console.ReadLine();
+                Box<string> box = new Box<string>(value);
+
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
